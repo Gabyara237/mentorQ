620eab11d316a10e8c62bcf2e443ca8bf2c264bf
Gabriela Araujo <gabyara237@gmail.com>
2026-01-01 14:47:45 -0800

    feat: Add Ticket schemas

---
+++ app/schemas/ticket.py
@@ -0,0 +1,25 @@
+
+from datetime import datetime
+from pydantic import BaseModel
+
+from app.models.ticket import TicketStatus
+
+
+class TicketCreate(BaseModel):
+    title: str
+    description: str
+
+class TicketResponse(BaseModel):
+    id: int
+    title: str
+    description:str
+    status: TicketStatus
+    student_id:int
+    assigned_mentor_id: int | None
+    solution: str | None
+    created_at: datetime 
+    assigned_at: datetime | None  
+    resolved_at: datetime | None  
+    closed_at: datetime | None   
+
+    
