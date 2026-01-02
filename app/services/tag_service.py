e34df025823b06fdc11eb2dc0754b0efbbde2b75
Gabriela Araujo <gabyara237@gmail.com>
2026-01-01 23:01:17 -0800

    feat: Add Tag service

---
+++ app/services/tag_service.py
@@ -0,0 +1,45 @@
+
+from fastapi import HTTPException
+from sqlmodel import Session, select
+
+from app.models.tag import Tag
+
+
+class TagService:
+
+    @staticmethod
+    def create_tag(session: Session, name: str)-> Tag:
+        
+        normalized_name = name.strip().lower()
+        query = select(Tag).where(Tag.name == normalized_name)
+        tag = session.exec(query).first()
+
+        if tag:
+            raise HTTPException(status_code=409, detail="Tag already registered")
+        
+        new_tag = Tag(
+            name= normalized_name
+        )
+
+        session.add(new_tag)
+        session.commit()
+        session.refresh(new_tag)
+
+        return new_tag
+    
+    @staticmethod
+    def get_all_tags(session: Session)->list[Tag]:
+        query = select(Tag)
+        tags = session.exec(query).all()
+        return tags
+    
+    @staticmethod
+    def get_or_create_tag(session:Session, name: str)-> Tag:
+        normalized_name = name.strip().lower()
+        query= select(Tag).where(Tag.name== normalized_name)
+        tag = session.exec(query).first()
+
+        if not tag:
+          tag=  TagService.create_tag(session,name)
+        
+        return tag
